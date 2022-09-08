b6b1d1749a9c92cd18f6564f999ded7e19738a9d
Panda15x <chayanmandal154@gmail.com>
2022-09-09 02:32:01 +0530

    Created a seperate page to display the artwork

---
+++ artwork.js
@@ -0,0 +1,24 @@
+const urlParams = new URLSearchParams(location.search);
+const myParam = urlParams.get('objectID');
+const contentBox = document.getElementById('content-box');
+
+async function getArtworkData(param) {
+    console.log(param);
+    let url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${param}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+async function showArtwork(param) {
+    const data = await getArtworkData(param);
+    document.title = data.title;
+    return `<h1>${data.title}</h1>`;
+}
+
+// contentBox.innerHTML = await showArtwork(myParam);
+window.onload = async (event) => {
+    contentBox.innerHTML = await showArtwork(myParam);
+    console.log('page is fully loaded');
+};
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const input_field = document.getElementById('input-field');
 const btn_search = document.getElementById('search');
 const results = document.getElementById('results');
 
-async function getArtwork(url) {
+async function getResults(url) {
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -18,7 +18,8 @@ function toPage(ID) {
 function presentResults(data) {
     let result = '';
     let results = data['objectIDs'].map((e) => {
-        return `<a href='#' onclick='toPage(${e})'>${e}</a>`;
+        let href = `/artwork.html?objectID=${e}`;
+        return `<a href='${href}' target='_blank'>${e}</a>`;
     });
     for (const i of results) {
         result = result + i;
@@ -30,8 +31,9 @@ function presentResults(data) {
 
 btn_search.addEventListener('click', async () => {
     let query = document.getElementById('input-field').value;
-    let url = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`;
-    let data = await getArtwork(url);
+    let url = `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${query}`;
+    let data = await getResults(url);
     console.log(data);
+    results.innerHTML = '';
     results.innerHTML = presentResults(data);
 });
